93fb8241329ee69865d0f20c3ff1c44b076b89c6
Gritzaev Nikita <gritzaevnikita@gmail.com>
2022-10-16 23:13:23 +0700

    feat: - структурировал css, js; - добавил верстку для деталей фильма, функции, выполняющие запрос и отображающие информацию;

---
+++ js/movie.js
@@ -0,0 +1,33 @@
+let api_url = "https://react-midterm.kreosoft.space/api";
+let movieDetails = new Object();
+
+async function getMovieDetails(filmId) {
+    let response = await fetch(`${api_url}/movies/details/${filmId}`);
+    if (response.ok) {
+        let json = await response.json();
+        movieDetails = json;
+        return json;
+    } else {
+        return new Object();
+    }
+}
+
+async function showDetails(filmId) {
+    let film = await getMovieDetails(filmId);
+    if (film) {
+        document.getElementById("name").textContent = `${film.name} (${film.year})`;
+        document.getElementById("time").textContent = film.time;
+        document.getElementById("tagline").textContent = `«${film.tagline}»`;
+        document.getElementById("director").textContent = film.director;
+        document.getElementById("budget").textContent = film.budget.toLocaleString('ru');
+        document.getElementById("fees").textContent = film.fees.toLocaleString('ru');
+        document.getElementById("ageLimit").textContent = `${film.ageLimit}+`;
+        document.getElementById("year").textContent = film.year;
+        document.getElementById("country").textContent = film.country;
+        document.getElementById("genres").textContent = film.genres.reduce((previousValue, currentValue) =>
+        previousValue + ", " + currentValue.name, "").slice(2);
+        document.getElementById("poster").src = film.poster;
+    }
+}
+
+showDetails("22158c42-001a-40a3-a2a7-08d9b9f3d2a2")
